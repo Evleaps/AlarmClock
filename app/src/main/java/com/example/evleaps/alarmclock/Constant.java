a7867b76e98b84fd7ca9c53321a06564b13862e1
evleaps <aymaletdinov2012@gmail.com>
2017-09-19 02:45:03 +0300

    update apk

---
+++ app/src/main/java/com/example/evleaps/alarmclock/Constant.java
@@ -0,0 +1,12 @@
+package com.example.evleaps.alarmclock;
+
+/**
+ * Constant for {@link SelectClock}
+ */
+
+public class Constant {
+    protected static final String SAVE_DATE_CLOCK = "save_date_newAlarmClock";
+    protected static final String DATE_FROM_TIMEPICKER = "date_timePicker";
+    protected static final int SIZE_HEIGHT_BUTTON = 350;
+    protected static final int SIZE_TEXT = 24;
+}
